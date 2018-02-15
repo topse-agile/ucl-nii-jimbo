ce18c5c4447e651bd8e8f7d1ed1440be29af575f
HaradaTomohiro <taizo.bontakun.19850103@gmail.com>
2018-02-15 14:56:10 +0900

    add inputFiletest

---
+++ src/test/java/com/fr_soft/demos/rfsample/InputFileTest.java
@@ -0,0 +1,23 @@
+package com.fr_soft.demos.rfsample;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+public class InputFileTest {
+
+	@Test
+	public void inputFileTest() {
+		DB db = new DB();
+		String filename = "hoge";
+		Map<String, Integer> stocks = db.imputfile(filename);
+		
+		assertThat(stocks.get("items"), is(10));
+
+	}
+
+}
